1c6c7c35f8adf147574cfa3a559449bac9cbce3d
jdp-pub <jacobdpurcell@outlook.com>
2024-01-08 10:30:52 -0500

    deleted:    axc/2023-12-18/math/differential_equation_phase_diagrams.pdf 	deleted:    axc/2023-12-18/math/percolation_theory.pdf 	deleted:    axc/2023-12-18/physics/Characterizing_post-selected_quantum_dynamics_and_its_classical_counterparts.pdf 	deleted:    axc/2023-12-18/physics/Spatio-temporal_correlations_quantum_mechanics.pdf 	deleted:    axc/2023-12-18/physics/quantum_computer_speed_limits.pdf 	deleted:    axc/2023-12-18/physics_boson-gate.pdf             clean
    	new file:   axc/config.py
    	modified:   axc/main.py
                FUNCTIONS: starts a passive observer that syncs with github repo
                TODO: automate a configuration process

--- axc/main.py
+++ axc/main.py
@@ -1,39 +1,112 @@
-import requests
-import axc
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+import sys
+import config as c
+import subprocess
+
+def posixcmd(cmd: str):
+    try:
+        output = subprocess.run(cmd, shell=True, text=True)
+        print(f"{cmd}: {output}")
+        return [output, 0]
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command {cmd}: {e}")
+        return ["error", e.returncode]
+
+def crashlog(e):
+    #write crash report to common place
+    crash_report = f"{time.now()}"
+    return e[1]
+
+def ecode(errc, cout):
+    if cout[1] in errc: return 0
+
+    print(f"Fatal error, crashlog generated in {c.ipath}crashlogs...")
+    print(f"Fatal error, please send crashlog to code administrator...")
+    sys.exit(crashlog(cout))
 
 
 
+class gitsync(FileSystemEventHandler):
+    def __init__(self, conf):
+        self.conf = conf
 
-def main():
-    # select topics
-    catl = ["physics", "math"]
+    def on_modified(self, event):
+        cmd = f"git checkout {self.conf.branch} -- {self.conf.rpath}"
+        cout = posixcmd(cmd)
+        ecode(c.codes, cout)
+        time.sleep(1)
+            
+        cmd = f"git -C {self.conf.rpath} add ."
+        cout = posixcmd(cmd)
+        ecode(c.codes, cout)
+        time.sleep(1)
 
-    top1 = [
-        "quantum computer speed limits",
-        "Characterizing post-selected quantum dynamics and its classical counterparts",
-        "Spatio-temporal correlations quantum mechanics"
-        ]
+        cmd = f"git -C {self.conf.rpath} commit -m \"Autosync\""
+        cout = posixcmd(cmd)
+        ecode(c.codes, cout)
+        time.sleep(1)
 
-    top2 = [
-        "percolation theory",
-        "differential equation phase diagrams",
-    ]
+        cmd = f"git -C {self.conf.rpath} push origin {self.conf.branch}"
+        cout = posixcmd(cmd)
+        ecode(c.codes, cout)
+        time.sleep(1)
+        stop
 
-    keys = []
-
-    
-    keys = [(catl[0],i) for i in top1] + [(catl[1],i) for i in top2]
-
-
-    # get topic list
-    print("beggining query...")
-    a = axc.query(keys)
-    keys = axc.clean(keys, "_")
-
-    # write selected topics to pdf
-    print("\nexporting...")
-    axc.export(keys, a)
+        
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+
+    cmd = f"GITHUB_ACCESS_TOKEN={c.gtoken}"
+    cout = posixcmd(cmd)
+    ecode(c.codes, cout)
+
+    print(cout)
+
+
+    stop
+
+
+
+    confl = []
+
+    for r in c.repo:
+        confl.append(c.conf(r[0], r[1], r[3]))
+        
+        cmd = f"git ls-remote --heads {r[2]}"
+        output = subprocess.check_output(cmd, shell=True, text=True)
+        if r[1] in output:
+            print(f"Working branch found for {r[2]}...")
+        else:
+            print(f"Creating working branch for {r[2]}...")
+            cmd = f"cd {r[0]} && git checkout -b {r[1]}"
+            try:
+                output = subprocess.check_output(cmd, shell=True, text=True)
+                print(f"{r[2]} branch {r[1]} created...", output)
+            except subprocess.CalledProcessError as e:
+
+                if e.returncode == 128:
+                    print(f"New branch {r[1]} in {r[2]} local only, branch will be created remotely after next push...")
+
+                print("Error executing command:", e)
+
+        time.sleep(1)
+
+    stop
+
+
+    for i in confl:
+        event_handler = gitsync(i)
+        observer = Observer()
+        observer.schedule(event_handler, path=i.rpath, recursive=True)
+        observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
